6332e9e9c486ba2dae4334ca3f11c50a946bfdfc
joo <yoasnevilsandy@gmail.com>
2025-06-26 09:01:14 +0700

    add + fix bug
    add tab profile itch.io
    fix bug scroll
    change button color + hover

--- scroll.js
+++ scroll.js
@@ -1,9 +1,28 @@
-document.querySelectorAll('nav a').forEach(anchor => {
+/*document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});*/
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+backToTopButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     });
 });
